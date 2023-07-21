a6667c117a207c2b00415328328e925fe4a47d98
queso65 <81798262+queso65@users.noreply.github.com>
2023-07-21 22:08:56 +1000

    Page switching

---
+++ src/components/StoreNumberpages.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="pages">
+        <div class="page__left"></div>
+        <a  v-for="page in totalPages+1" :key="page" :data-id="page" class="page page-text" @click="changePage">{{ page }}</a>
+        <div class="page__right"></div>
+    </div>
+</template>
+
+<script>
+export default {
+   props: {
+    totalPages: Number,
+   },
+   methods: {
+    changePage(event){
+        var id = event.target.getAttribute('data-id');
+        this.$emit('changePage', id)
+    }
+   }
+}
+</script>
+
+<style scoped>
+.pages{
+	display:flex;
+	margin-top: 30px;
+	justify-content: center;
+}
+
+.page__left{
+	font-size:11px;
+	width: 25px;
+	border-left:none;
+	border-radius: 10px 0  0 10px;
+}
+
+.page__right{
+	font-size:11px;
+	width: 25px;
+	border-radius: 0 10px  10px 0;
+}
+
+.page__right,
+.page__left,
+.page{
+	width: 27px;
+	height: 27px;
+	background: #e5e5e5;
+	border-left:  3px solid #cfcfcf;
+	box-shadow: 2px 2px 4px 1px rgba(0, 0, 0, 0.2);
+	align-items:center;
+	justify-content:center;
+	display:flex;
+}
+
+.page-text{
+	font-family: Arial, sans-serif;
+	font-size: 15px;
+	color:#8c8c8c;
+}
+</style>
--- src/pages/StorePage.vue
+++ src/pages/StorePage.vue
@@ -5,16 +5,9 @@
       <h1 class="сategory">Все товары</h1>
   
       <!--  На одной странице по 9 товаров-->
-      <store-list :posts="posts" @remove="removePost" />
-  
-      <div class="pages">
-        <div class="page__left"></div>
-        <a data-id="" class="page page-text" href="page.html">1</a>
-        <a data-id="" class="page page-text" href="page.html">2</a>
-        <a data-id="" class="page page-text" href="page.html">3</a>
-        <a data-id="" class="page page-text" href="last_page.html">4</a>
-        <div class="page__right"></div>
-      </div>
+      <store-list :posts="posts[page]" @remove="removePost" />
+
+      <store-numberpages :totalPages="numberPages()" @changePage="changePage"></store-numberpages>
   
       <store-footer></store-footer>
   </template>
@@ -24,17 +17,19 @@
   import StoreHeader from '@/components/StoreHeader.vue'
   import StoreCatalog from '@/components/StoreCatalog.vue'
   import StoreFooter from '@/components/StoreFooter.vue'
+  import StoreNumberpages from '@/components/StoreNumberpages.vue'
   export default {
     components: {
       StoreList,
       StoreHeader,
       StoreCatalog,
-      StoreFooter
+      StoreFooter,
+      StoreNumberpages
     },
     data() {
       return {
         posts: [
-          { id: 1, img: "http://1001goroskop.ru/img/gadanie/derevo_zh/_derevo.jpg", title: "футболка", price: "200" },
+          [{ id: 1, img: "http://1001goroskop.ru/img/gadanie/derevo_zh/_derevo.jpg", title: "футболка", price: "200" },
           { id: 2, img: "src/assets/img/T-shirt.jpg", title: "футболка", price: "200" },
           { id: 3, img: "/../assets/img/logo.png", title: "футболка", price: "200" },
           { id: 4, img: "../assets/img/T-shirt.jpg", title: "футболка", price: "200" },
@@ -42,14 +37,32 @@
           { id: 6, img: "@/assets/img/T-shirt.jpg", title: "футболка", price: "200" },
           { id: 7, img: "@/assets/img/T-shirt.jpg", title: "футболка", price: "200" },
           { id: 8, img: "@/assets/img/T-shirt.jpg", title: "футболка", price: "200" },
-          { id: 9, img: "@/assets/img/T-shirt.jpg", title: "футболка", price: "200" },
+          { id: 9, img: "@/assets/img/T-shirt.jpg", title: "футболка", price: "200" }],
+          [{ id: 10, img: "/../assets/img/logo.png", title: "футболка", price: "200" },
+          { id: 11, img: "../assets/img/T-shirt.jpg", title: "футболка", price: "200" },
+          { id: 12, img: "folder/assets/img/T-shirt.jpg", title: "футболка", price: "200" },
+          { id: 13, img: "@/assets/img/T-shirt.jpg", title: "футболка", price: "200" },
+          { id: 14, img: "@/assets/img/T-shirt.jpg", title: "футболка", price: "200" },
+          { id: 15, img: "@/assets/img/T-shirt.jpg", title: "футболка", price: "200" },
+          { id: 16, img: "@/assets/img/T-shirt.jpg", title: "футболка", price: "200" },]
         ],
+        page: 0,
+        limit: 9,
+        totalPages: 0
       }
     },
     methods: {
       removePost(post) {
         this.posts = this.posts.filter(p => p.id !== post.id)
+        this.numberPages()
       },
+      numberPages() {
+        this.totalPages = Math.ceil(this.posts.length / this.limit)
+        return this.totalPages
+      },
+      changePage(page){
+        this.page = (page-1);
+      }
     }
   }
   
